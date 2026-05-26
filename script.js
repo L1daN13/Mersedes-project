3a431df5b7f68d885a8185606f4403b093382d88
L1daN13 <150241145+L1daN13@users.noreply.github.com>
2026-05-26 16:33:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const cars = [
   { name: "AMG GT", price: "$185,000" },
   { name: "S-Class", price: "$142,000" },
-  { name: "G-Class", price: "$220,000" }
+  { name: "G-Class", price: "$220,000" },
+  { name: "EQS 580", price: "$138,000" }
 ];
 
 const grid = document.getElementById("carsGrid");
 
 cars.forEach(car => {
   const div = document.createElement("div");
-  div.style.padding = "15px";
-  div.style.background = "#1a1a1a";
-  div.style.borderRadius = "10px";
+  div.className = "car";
 
   div.innerHTML = `
     <h3>${car.name}</h3>

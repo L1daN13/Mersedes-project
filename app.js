9bd8ca30576452ce9096b66ac307b69bc2e5bb98
L1daN13 <150241145+L1daN13@users.noreply.github.com>
2026-05-26 16:49:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const grid = document.getElementById("grid");
 
+cars.forEach(car => {
+  const div = document.createElement("div");
+  div.className = "card";
+
+  div.innerHTML = `
+    <img src="${car.images[0]}">
+    <h3>${car.title}</h3>
+    <p>$${car.price}</p>
+  `;
+
+  div.onclick = () => {
+    window.location.href = `car.html?id=${car.id}`;
+  };
+
+  grid.appendChild(div);
+});

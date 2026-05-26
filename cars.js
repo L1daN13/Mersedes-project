2b4d568264dcf57de2ef6f205f4656894aee3ac7
L1daN13 <150241145+L1daN13@users.noreply.github.com>
2026-05-26 16:49:19 +0300

    Update cars.js

--- cars.js
+++ cars.js
@@ -1,1 +1,42 @@
-
+const cars = [
+  {
+    id: "amg-gt",
+    title: "AMG GT 63 S",
+    price: 185000,
+    type: "sport",
+    desc: "630 HP V8 Biturbo",
+    images: [
+      "https://upload.wikimedia.org/wikipedia/commons/3/3b/2019_Mercedes-AMG_GT_4-Door.jpg"
+    ]
+  },
+  {
+    id: "s-class",
+    title: "S-Class",
+    price: 140000,
+    type: "sedan",
+    desc: "Luxury flagship",
+    images: [
+      "https://upload.wikimedia.org/wikipedia/commons/3/3e/Mercedes-Benz_W223.jpg"
+    ]
+  },
+  {
+    id: "g-class",
+    title: "G-Class",
+    price: 220000,
+    type: "suv",
+    desc: "Legendary SUV",
+    images: [
+      "https://upload.wikimedia.org/wikipedia/commons/3/3f/Mercedes_G_class.jpg"
+    ]
+  },
+  {
+    id: "eqs",
+    title: "EQS",
+    price: 135000,
+    type: "electric",
+    desc: "Electric flagship",
+    images: [
+      "https://upload.wikimedia.org/wikipedia/commons/3/3d/Mercedes_EQS.jpg"
+    ]
+  }
+];
